d1068d6a84b49f80ed7d1ccec77573d3c289bfb6
Kartheek Vadde <kartik180595@gmail.com>
2020-05-05 18:36:32 +0530

    Adding Package(morestrings)

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/kartheekvadde/accuknox/morestrings"
+)
 
 func main() {
 	fmt.Println("Hello, world.")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 }
