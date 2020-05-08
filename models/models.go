ea67edb3fe2d3a0180c023e833fedc164d35eb42
Kartheek Vadde <kartik180595@gmail.com>
2020-05-08 15:32:29 +0530

    Adding Models (Pod and ResponsePods)

--- models/models.go
+++ models/models.go
@@ -27,3 +27,17 @@ type Node struct {
 type ResponseNodes struct {
 	Nodes []Node `json:"nodes"`
 }
+type Pod struct {
+	ID          int    `json:"id"`
+	OrgID       int    `json:"org_id"`
+	UserID      int    `json:"user_id"`
+	PodName     string `json:"pod_name"`
+	ClusterName string `json:"cluster_name"`
+	NodeCount   int    `json:"node_count"`
+	Location    string `json:"location"`
+	PolicyID    int    `json:"policy_id"`
+	Status      string `json:"status"`
+}
+type ResponsePods struct {
+	Pods []Pod `json:"pods"`
+}
