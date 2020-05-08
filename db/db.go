f71e56f0c79a8af49fc8f7404a2630e11e8fb17b
Kartheek Vadde <kartik180595@gmail.com>
2020-05-08 15:27:47 +0530

    Updating DB Connection

--- db/db.go
+++ db/db.go
@@ -7,9 +7,9 @@ func DbConn() (db *sql.DB) {
 	// dbUser := "root"
 	// dbPass := "1234"
 	// dbName := "accuknox"
+	// db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
 	connString := "root:1234@tcp(localhost:3306)/accuknox"
 	db, err := sql.Open("mysql", connString)
-	// db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
 
 	if err != nil {
 		panic(err.Error())
